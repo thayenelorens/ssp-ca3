8e171dd4d5b394c58bcc6bb68c044d8f4ce70900
thayenelorens <thayenelorens@gmail.com>
2022-01-03 17:52:09 +0000

    Adding a jQuery function to this file

--- ThaysProject/js/myQuery.js
+++ ThaysProject/js/myQuery.js
@@ -18,3 +18,13 @@ $(document).ready(function(){
         $("#movieTrailer").attr('src', url);
     });
    
+/**
+ * In this function once the document is ready or loaded and the user hovers the cursor on the button whose id is "contact1",
+ * the 'panel' element will slowly slide down, making it interactive.
+ * This function was sourced from: https://www.w3schools.com/jquery/tryit.asp?filename=tryjquery_slide_down
+ */
+ $(document).ready(function () {
+    $("#contact").mouseenter(function () {
+      $("#panel").slideDown("slow");
+    });
+  });
